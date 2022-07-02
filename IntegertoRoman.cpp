f0aed8655e4084b323c3eb645c46f891da0b5722
Abhigya Jain <64054535+Abhigya3159@users.noreply.github.com>
2022-07-02 23:16:34 +0800

    Update IntegertoRoman.cpp

--- IntegertoRoman.cpp
+++ IntegertoRoman.cpp
@@ -1,82 +1,41 @@
+//MY SOLUTION
 string intToRoman(int num) {
         string romannumeral;
-        int rem = 0, cnt = 0;
-        list<int> ilst;
-        while (num != 0){
-            rem = num%10;
-            ilst.push_front(rem*pow(10, cnt));
-            num = num/10;
-            cnt++;
-        }
-        for (auto b = ilst.begin(); b != ilst.end(); ++b){
-            if (*b == 1) romannumeral += "I";
-            if (*b == 2) romannumeral += "II";
-            if (*b == 3) romannumeral += "III";
-            if (*b == 4) romannumeral += "IV";
-            if (*b == 5) romannumeral += "V";
-            if (*b == 6) romannumeral += "VI";
-            if (*b == 7) romannumeral += "VII";
-            if (*b == 8) romannumeral += "VIII";
-            if (*b == 9) romannumeral += "IX";
-            if (*b == 10) romannumeral += "X";
-            if (*b == 20) romannumeral += "XX";
-            if (*b == 30) romannumeral += "XXX";
-            if (*b == 40) romannumeral += "XL";
-            if (*b == 50) romannumeral += "L";
-            if (*b == 60) romannumeral += "LX";
-            if (*b == 70) romannumeral += "LXX";
-            if (*b == 80) romannumeral += "LXXX";
-            if (*b == 90) romannumeral += "XC";
-            if (*b == 100) romannumeral += "C";
-            if (*b == 200) romannumeral += "CC";
-            if (*b == 300) romannumeral += "CCC";
-            if (*b == 400) romannumeral += "CD";
-            if (*b == 500) romannumeral += "D";
-            if (*b == 600) romannumeral += "DC";
-            if (*b == 700) romannumeral += "DCC";
-            if (*b == 800) romannumeral += "DCCC";
-            if (*b == 900) romannumeral += "CM";
-            if (*b == 1000) romannumeral += "M";
-            if (*b == 2000) romannumeral += "MM";
-            if (*b == 3000) romannumeral += "MMM";
-        }
-        if (ilst.size() == 4){
-            
-        }
-        return romannumeral;
-}
-
-
-class Solution {
-public:
-    string intToRoman(int num) {
-        string romannumeral;
-        int rem = 0, cnt = 0;
+        int rem = 0;
         vector<int> ivec;
-        /*vector<string> ones{"I", "II", "III", "IV", "V"
-                            "VI", "VII", "VIII", "IX"};
-        vector<string> tens{"X", "XX", "XXX", "XL", "L"
-                            "LX", "LXX", "LXXX", "XC"};
-        vector<string> hundreds{"C", "CC", "CCC", "CD", "D"
-                            "DC", "DCC", "DCCC", "CM"};
-        vector<string> thousands{"M", "MM", "MMM" };*/
-        string romandigits[4][9] = {{"I", "II", "III", "IV", "V"
-                            "VI", "VII", "VIII", "IX"}, {"X", "XX", "XXX", "XL", "L"
-                            "LX", "LXX", "LXXX", "XC"}, {"C", "CC", "CCC", "CD", "D"
-                            "DC", "DCC", "DCCC", "CM"}, {"M", "MM", "MMM" }};
+        vector<vector<string>> romandigits{{"", "I", "II", "III", "IV", "V",
+                            "VI", "VII", "VIII", "IX"}, {"", "X", "XX", "XXX", "XL", "L",
+                            "LX", "LXX", "LXXX", "XC"}, {"", "C", "CC", "CCC", "CD", "D",
+                            "DC", "DCC", "DCCC", "CM"}, {"", "M", "MM", "MMM"}};
         while (num != 0){
             rem = num%10;
             ivec.push_back(rem);
             num = num/10;
-            cnt++;
         }
-        romannumeral += romandigits[0][ivec[0]];
-        auto p = romannumeral.begin();
-        if (ivec.size() > 1){
-            for (int i = 1; i != ivec.size(); ++i){
-                romannumeral.insert(p, romandigits[i][ivec[i]]);
+        vector<int> ivec2(ivec.rbegin(), ivec.rend());
+            for (int i = 0, j = ivec2.size()-1; i != ivec2.size(), j >= 0 ; ++i, --j){
+                romannumeral += romandigits[j][ivec2[i]];
+            }
+        return romannumeral;
+}
+
+//BETTER SOLUTION
+string intToRoman(int num) {
+    vector<int> values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    vector<string> roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    
+    string ans;
+    
+    for(int i=0;i<values.size();i++){
+        while(num){
+            if(values[i]>num){
+                break;
+            }
+            else{
+                ans+=roman[i];
+                num-=values[i];
             }
         }
-        return romannumeral;
     }
-};
+    return ans;
+}
