7c419ba81c49ca0990eb031c0daa0e1baa19ba08
Abhigya Jain <64054535+Abhigya3159@users.noreply.github.com>
2022-07-02 19:33:53 +0800

    Create RomantoInteger.cpp

---
+++ RomantoInteger.cpp
@@ -0,0 +1,32 @@
+int romanToInt(string s) {
+        int sum = 0;
+        for (auto b = s.begin(); b != s.end(); ++b){
+            if (*b == 'I')
+                sum += 1;
+            if (*b == 'V')
+                sum += 5;
+            if (*b == 'X')
+                sum += 10;
+            if (*b == 'L')
+                sum += 50;
+            if (*b == 'C')
+                sum += 100;
+            if (*b == 'D')
+                sum += 500;
+            if (*b == 'M')
+                sum += 1000;
+        }
+        if (s.find("IV") < s.size())
+            sum = sum - 2;
+        if (s.find("IX") < s.size())
+            sum = sum - 2;
+        if (s.find("XL") < s.size())
+            sum = sum - 20;
+        if (s.find("XC") < s.size())
+            sum = sum - 20;
+        if (s.find("CD") < s.size())
+            sum = sum - 200;
+        if (s.find("CM") < s.size())
+            sum = sum - 200;
+        return sum;
+    }
