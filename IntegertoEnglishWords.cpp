07c442d842764702edb04f312ee949b1e0b91bb1
Abhigya Jain <64054535+Abhigya3159@users.noreply.github.com>
2022-07-04 11:49:54 +0800

    Update IntegertoEnglishWords.cpp

--- IntegertoEnglishWords.cpp
+++ IntegertoEnglishWords.cpp
@@ -1,3 +1,4 @@
+//MY SOLUTION
 class Solution {
 public:
     string hundreds(const string &val, const int &num){
@@ -149,5 +150,30 @@ public:
             }
         }
         return text;
+    }
+};
+
+//BEST SOLUTION
+class Solution {
+public:
+    string numberToWords(int num) {
+        string w1[] = {"", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine"};
+        string w2[] = {" Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen" };
+        string w3[] = {"",  "",  " Twen", " Thir", " For", " Fif", " Six", " Seven", " Eigh", " Nine" };
+        string e[] = {"", " Thousand", " Million", " Billion"};
+        
+        std::string s; int k=0;
+        while(num) {
+            std::string sub;
+            auto s4=num%1000, s3=s4/100, s2=s4%100;
+            if(s4) s.insert(0, e[k]);
+            if(s3) sub += w1[s3] + " Hundred";
+            if(s2) sub += s2<20? (s2<10? w1[s2%10]: w2[s2%10]): w3[s2/10%10]+"ty"+w1[s2%10];
+            s.insert(0, sub);
+            num /= 1000;
+            k++;
+        }
+        
+        return s.size()? std::string(s.begin()+1, s.end()): "Zero";
     }
 };
