b620c935d99a041896bb337c81a5d2893728e433
Abhigya Jain <64054535+Abhigya3159@users.noreply.github.com>
2022-07-04 12:03:44 +0800

    Update IntegertoEnglishWords.cpp

--- IntegertoEnglishWords.cpp
+++ IntegertoEnglishWords.cpp
@@ -19,10 +19,10 @@ public:
             else words = words + svec2[num/10];
         }
         if (val.size() == 3){
-            int sbscrpt = (num%100)/10; //tens digit
-            int sbscrpt2 = (num%100)%10; //ones digit
-            int sbscrpt3 = num/100; //hundreds digit
-            int sbscrpt4 = num%100; //For case where remainder is greater than 19
+            int sbscrpt = (num%100)/10;
+            int sbscrpt2 = (num%100)%10; 
+            int sbscrpt3 = num/100;
+            int sbscrpt4 = num%100; 
             if (sbscrpt == 0 && sbscrpt2 == 0 && sbscrpt3 == 0 && sbscrpt4 == 0) words += "";
             else if (sbscrpt == 0 && sbscrpt2 != 0) words = words + svec[sbscrpt3] + " Hundred " + svec[sbscrpt2];
             else if (sbscrpt != 0 && sbscrpt2 == 0) words = words + svec[sbscrpt3] + " Hundred " + svec2[sbscrpt];
@@ -37,7 +37,6 @@ public:
         string val = to_string(num), text, text2, text3, text4;
         vector<int> remainders;
         vector<string> remainderstrings;
-        int rem;
         if (val.size() <= 3) text = hundreds(val, num);
         if (val.size() > 3 && val.size() < 7){
             remainders.push_back(num/1000); 
@@ -56,29 +55,29 @@ public:
             }
         }
         if (val.size() >= 7 && val.size() < 10){
-            remainders.push_back(num/1000000); //millions digit
-            remainderstrings.push_back(to_string(num/1000000)); //millions
+            remainders.push_back(num/1000000);
+            remainderstrings.push_back(to_string(num/1000000)); 
             text = hundreds(remainderstrings[0], remainders[0]);
             int sbscrpt = (num%1000000)/1000, sbscrpt2 = (num%1000000)%1000;
             if ((sbscrpt == 0) && (sbscrpt2 == 0)){
                 text = text + " Million";
             }
             else if ((sbscrpt != 0) && (sbscrpt2 == 0)){
-                remainders.push_back(sbscrpt); //thousands
+                remainders.push_back(sbscrpt); 
                 remainderstrings.push_back(to_string(sbscrpt));
                 text2 = hundreds(remainderstrings[1], remainders[1]);
                 text = text + " Million " + text2 + " Thousand";
             }
             else if ((sbscrpt == 0) && (sbscrpt2 != 0)){
-                remainders.push_back(sbscrpt2); //hundreds
+                remainders.push_back(sbscrpt2); 
                 remainderstrings.push_back(to_string(sbscrpt2));
                 text3 = hundreds(remainderstrings[1], remainders[1]);
                 text = text + " Million " + text3;
             }
             else {
-                remainders.push_back(sbscrpt); //thousands
+                remainders.push_back(sbscrpt); 
                 remainderstrings.push_back(to_string(sbscrpt));
-                remainders.push_back(sbscrpt2); //hundreds
+                remainders.push_back(sbscrpt2);
                 remainderstrings.push_back(to_string(sbscrpt2));
                 text2 = hundreds(remainderstrings[1], remainders[1]);
                 text3 = hundreds(remainderstrings[2], remainders[2]);
@@ -86,32 +85,30 @@ public:
             }
         } 
         if (val.size() == 10){
-            remainders.push_back(num/1000000000); //billions
+            remainders.push_back(num/1000000000); 
             remainderstrings.push_back(to_string(num/1000000000));
             text = hundreds(remainderstrings[0], remainders[0]);
-            int sbscrpt = (num%1000000000)/1000000; //millions
-            int sbscrpt2 = ((num%1000000000)%1000000)/1000; //thousands
-            int sbscrpt3 = ((num%1000000000)%1000000)%1000; //hundreds
+            int sbscrpt = (num%1000000000)/1000000, sbscrpt2 = ((num%1000000000)%1000000)/1000, sbscrpt3 = ((num%1000000000)%1000000)%1000; 
             if (sbscrpt == 0){
                 if ((sbscrpt2 == 0) && (sbscrpt3 == 0)){
                     text = text + " Billion";
                 }
                 else if ((sbscrpt2 != 0) && (sbscrpt3 == 0)){
-                    remainders.push_back(sbscrpt2); //thousands
+                    remainders.push_back(sbscrpt2); 
                     remainderstrings.push_back(to_string(sbscrpt2));
                     text2 = hundreds(remainderstrings[1], remainders[1]);
                     text = text + " Billion " + text2 + " Thousand";
                 }
                 else if ((sbscrpt2 == 0) && (sbscrpt3 != 0)){
-                    remainders.push_back(sbscrpt3); //hundreds
+                    remainders.push_back(sbscrpt3); 
                     remainderstrings.push_back(to_string(sbscrpt3));
                     text3 = hundreds(remainderstrings[1], remainders[1]);
                     text = text + " Billion " + text3;
                 }
                 else {
-                    remainders.push_back(sbscrpt2); //thousands
+                    remainders.push_back(sbscrpt2); 
                     remainderstrings.push_back(to_string(sbscrpt2));
-                    remainders.push_back(sbscrpt3); //hundreds
+                    remainders.push_back(sbscrpt3); 
                     remainderstrings.push_back(to_string(sbscrpt3));
                     text2 = hundreds(remainderstrings[1], remainders[1]);
                     text3 = hundreds(remainderstrings[2], remainders[2]);
@@ -127,21 +124,21 @@ public:
                     text = text + text4 + " Million";
                 }
                 else if ((sbscrpt2 != 0) && (sbscrpt3 == 0)){
-                    remainders.push_back(sbscrpt2); //thousands
+                    remainders.push_back(sbscrpt2); 
                     remainderstrings.push_back(to_string(sbscrpt2));
                     text2 = hundreds(remainderstrings[2], remainders[2]);
                     text = text + text4 + " Million " + text2 + " Thousand";
                 }
                 else if ((sbscrpt2 == 0) && (sbscrpt3 != 0)){
-                    remainders.push_back(sbscrpt3); //hundreds
+                    remainders.push_back(sbscrpt3); 
                     remainderstrings.push_back(to_string(sbscrpt3));
                     text3 = hundreds(remainderstrings[2], remainders[2]);
                     text = text + text4 + " Million " + text3;
                 }
                 else {
-                    remainders.push_back(sbscrpt2); //thousands
+                    remainders.push_back(sbscrpt2); 
                     remainderstrings.push_back(to_string(sbscrpt2));
-                    remainders.push_back(sbscrpt3); //hundreds
+                    remainders.push_back(sbscrpt3); 
                     remainderstrings.push_back(to_string(sbscrpt3));
                     text2 = hundreds(remainderstrings[2], remainders[2]);
                     text3 = hundreds(remainderstrings[3], remainders[3]);
@@ -165,8 +162,8 @@ public:
         std::string s; int k=0;
         while(num) {
             std::string sub;
-            auto s4=num%1000, s3=s4/100, s2=s4%100;
-            if(s4) s.insert(0, e[k]);
+            auto s4=num%1000, s3=s4/100, s2=s4%100; //Example 123456: s4=456, s3=4, s2=56
+            if(s4) s.insert(0, e[k]); 
             if(s3) sub += w1[s3] + " Hundred";
             if(s2) sub += s2<20? (s2<10? w1[s2%10]: w2[s2%10]): w3[s2/10%10]+"ty"+w1[s2%10];
             s.insert(0, sub);
